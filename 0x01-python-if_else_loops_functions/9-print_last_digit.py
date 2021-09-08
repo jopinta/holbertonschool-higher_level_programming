5d4bcae69a2e4f35b09d7abaa99d2a6ec639857d
jopinta <tatinfa@yahoo.com>
2021-09-08 12:45:44 -0700

    pld

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-def ptint_last_digit(number):
-    if number < 0:
-        number = number * -1
-    res = number % 10
-    print("{:d}",format(res), end="")
-    return res
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return abs(number) % 10
