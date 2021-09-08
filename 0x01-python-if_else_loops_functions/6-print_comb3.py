92148b5d0359397ac4c82b6494ecbe1a0afd1fd4
jopinta <tatinfa@yahoo.com>
2021-09-08 11:58:09 -0700

    pld helped with i > j

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(10):
-    for j in range(i+1, 10):
+    for j in range(10):
         if i > j or (i == 8 and j == 9):
             continue
         print("{:d}{:d}".format(i, j), end=', ')
