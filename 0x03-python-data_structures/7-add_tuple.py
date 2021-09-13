d7ffcdac766681afd3a6e4477d6f6736db4a970c
jopinta <tatinfa@yahoo.com>
2021-09-13 15:57:04 -0700

    pep8 corrrcted

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,12 +5,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tmp = [0, 0]
     a = list(tuple_a)
     b = list(tuple_b)
-
-
-    for i in range(0,2:)
+    for i in range(0, 2):
         if (i >= len(a)):
             a.append(0)
         if (i >= len(b)):
-        tmp[i] = a[i] + b[i]
+            tmp[i] = a[i] + b[i]
 
     return tuple(tmp)
