ebc77e480676001f706d1253390cad92c712bd7f
jopinta <tatinfa@yahoo.com>
2021-09-13 15:52:22 -0700

    using tmp to add

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tmp = [0, 0]
+    a = list(tuple_a)
+    b = list(tuple_b)
+
+
+    for i in range(0,2:)
+        if (i >= len(a)):
+            a.append(0)
+        if (i >= len(b)):
+        tmp[i] = a[i] + b[i]
+
+    return tuple(tmp)
