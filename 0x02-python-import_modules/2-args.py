737e5f3e9067602f57a7403a460970e7db67fd26
jopinta <tatinfa@yahoo.com>
2021-09-14 15:06:33 -0700

    pld fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         exit()
     print("{:d} arguments:".format(argvec - 1))
     for i in range(1, argvec):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    print("{:d}: {}".format(i, sys.argv[i]))
