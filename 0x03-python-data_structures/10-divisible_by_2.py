9e148a345adb1a1741951f1b091f6b8257109113
jopinta <tatinfa@yahoo.com>
2021-09-13 18:13:24 -0700

    true false cambiados

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     tmp = []
     for i in my_list:
         if i % 2 == 0:
-            tmp.append(False)
-        else:
             tmp.append(True)
+        else:
+            tmp.append(False)
             return tmp
