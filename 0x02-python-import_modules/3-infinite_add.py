7bce7dcfbe90e5515cf707986d2b9788fdd90483
jopinta <tatinfa@yahoo.com>
2021-09-14 15:26:24 -0700

    pld helpéd

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(sys.argv)
     add = 0
-        for j in range(1, argc):
-            add = add + int(sys.argv[j])
+    for j in range(1, argc):
+        add = add + int(sys.argv[j])
     print("{:d}".format(add))
