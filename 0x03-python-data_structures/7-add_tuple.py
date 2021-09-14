94645b3eaaab5b1d90792dc9b596571f55c1b68c
jopinta <tatinfa@yahoo.com>
2021-09-14 11:11:46 -0700

    no menor o igual

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,9 +6,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a = list(tuple_a)
     b = list(tuple_b)
     for i in range(0, 2):
-        if (i >= len(a)):
+        if (i > len(a) - 1):
             a.append(0)
-        if (i >= len(b)):
+        if (i >len(b) - 1):
             b.append(0)
             tmp[i] = a[i] + b[i]
 
