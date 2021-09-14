73e4052038c15bf71c68d43407b40a1eef315e01
jopinta <tatinfa@yahoo.com>
2021-09-13 19:07:02 -0700

    adding parentesis en el if

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@
 def divisible_by_2(my_list=[]):
     tmp_ls = []
     for i in my_list:
-        if i % 2 == 0:
+        if (i % 2 == 0):
             tmp_ls.append(True)
         else:
             tmp_ls.append(False)
