b71e7d9d364740541df7947f6629c0c91dc6dfb8
jopinta <tatinfa@yahoo.com>
2021-09-14 16:10:47 -0700

    pld fix and done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print("{:d}".format(martix[i][j]), end='')
+            if (j != len(matrix[i]) - 1):
+                print(" ",end='')
+        print("")
