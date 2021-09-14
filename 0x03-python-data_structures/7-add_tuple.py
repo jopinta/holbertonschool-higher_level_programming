5541f1a66fe859dd545301f807ac61824739944e
jopinta <tatinfa@yahoo.com>
2021-09-14 10:53:38 -0700

    pld fixed

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,6 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if (i >= len(a)):
             a.append(0)
         if (i >= len(b)):
+            b.append(0)
             tmp[i] = a[i] + b[i]
 
     return tuple(tmp)
