25f3b5a95c5a863552e99a00f1211daf399b00e6
jopinta <tatinfa@yahoo.com>
2021-09-14 15:05:09 -0700

    pld fixed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+0;10;1c
+
+if __name__ == "__main__":
+    import sys
+    argvec = len(sys.argv)
+    if argvec == 1:
+        print("0 arguments.")
+        exit()
+    elif argvec == 2:
+        print("1 argument:")
+        print("{}: {}".format(1, sys.argv[1]))
+        exit()
+    print("{:d} arguments:".format(argvec - 1))
+    for i in range(1, argvec):
+        print("{:d}: {}".format(i, sys.argv[i]))
