ff4aec513e23906796bf77263b14d46f9d19b01b
jopinta <tatinfa@yahoo.com>
2021-09-15 08:44:08 -0700

    trying with if and size of square

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for i in range(0, len(matrix)):
+        new_matrix.append([])
+        for j in range(0, len(matrix[i])):
+            newmatrix[i.append(j)
+
+                      if (len(sys.argv) == 3):
+        height = sys.argv[1]
+        width = sys.argv[2]
+
+    else:
+        raise Exception()
