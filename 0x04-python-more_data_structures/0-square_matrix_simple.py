79d1f18e695ecfd6ab5a0bcc000686d6fa628b7a
jopinta <tatinfa@yahoo.com>
2021-09-15 16:11:10 -0700

    new matrix == matrix **2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,11 +7,9 @@ def square_matrix_simple(matrix=[]):
     for i in range(0, len(matrix)):
         new_matrix.append([])
         for j in range(0, len(matrix[i])):
-            newmatrix[i.append(j)
-
-                      if (len(sys.argv) == 3):
-        height = sys.argv[1]
-        width = sys.argv[2]
+            new_matrix[i].append(j)
+            new_matrix[i][j] = matrix[i][j] ** 2
 
     else:
         raise Exception()
+    return new_matrix
