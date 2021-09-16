803a8bdd9a7463a989d3113dc1a388d46c4ad512
jopinta <tatinfa@yahoo.com>
2021-09-15 18:29:33 -0700

    search n replace fixed with else

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,4 +6,6 @@ def serch_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             new_list.append(replace)
+        else:
+            new_list.append(i)
     return new_list
