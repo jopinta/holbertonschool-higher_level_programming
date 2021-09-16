4dc678b47c50e0f4b84a40de0007f8333d027ffe
jopinta <tatinfa@yahoo.com>
2021-09-15 19:43:08 -0700

    items added

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-
 def multiply_by_2(a_dictionary):
     new = {}
-    forx, y in a_dictionary
-    new[]x = y * 2
+    for x, y in a_dictionary.items():
+    new[x] = y * 2
     return new
