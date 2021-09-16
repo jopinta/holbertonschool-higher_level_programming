d922d696eb72bbe64ff71314c6b86bc11329ef42
jopinta <tatinfa@yahoo.com>
2021-09-15 17:18:51 -0700

    fixed to wrok in 8

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,6 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
+    new = sorted(a_dictionary.keys())
+    for i in new:
         print("{}: {}".format(i, a_dictionary[i]))
