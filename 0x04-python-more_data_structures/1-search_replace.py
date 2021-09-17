3305a0525829e95d5107baa1050ed14028378cf6
jopinta <tatinfa@yahoo.com>
2021-09-17 13:02:00 -0700

    pld

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-
 def serch_replace(my_list, search, replace):
     new_list = []
     for i in my_list:
-        if i == search:
+        if (i == search):
             new_list.append(replace)
         else:
             new_list.append(i)
