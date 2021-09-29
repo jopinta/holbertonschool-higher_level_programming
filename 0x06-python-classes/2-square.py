fdd8d837c7720b4021de9078da1fd2254f50eafb
jopinta <tatinfa@yahoo.com>
2021-09-28 19:22:45 -0700

    caps on S line 15

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,11 +7,11 @@ class Square:
     __size = None
 
     def __init__(self, size = 0):
-        """init with attributte size"""
+        """initialize with attributte size"""
         if type(size) is int:
             if size >= 0:
                 self.__size = size
             else:
-                raise ValueError("size must be >= 0")
+                raise ValueError("Size must be >= 0")
         else:
             raise TypeError("size must be an integer")
