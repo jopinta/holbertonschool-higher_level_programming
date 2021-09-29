bef2d8ea2e561dabdf9325985a40d3f4d931b7a1
jopinta <tatinfa@yahoo.com>
2021-09-28 17:07:01 -0700

    ted

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square defining the specialization"""
+
+
+class Square:
+    __size = None
+
+    def __init__(size):
+        """to initialize"""
+        self.__size = size
