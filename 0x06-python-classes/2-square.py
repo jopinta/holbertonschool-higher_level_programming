62dae3414a1bf5ecfb9f369d5de8673b2ee9211f
jopinta <tatinfa@yahoo.com>
2021-09-28 18:10:18 -0700

    one space and gets red line 15

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,6 +12,6 @@ class Square:
             if size >= 0:
                 self.__size = size
             else:
-                 raise ValueError("size must be >= 0")
+                raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
