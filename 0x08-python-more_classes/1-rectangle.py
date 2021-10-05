288beabd4732a048d747ed90ade3c8d4a58b9f35
jopinta <tatinfa@yahoo.com>
2021-10-04 18:35:45 -0700

    rectangle with instances

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+based on 0-rectangle.py
+"""
+
+class Rectangle:
+    """new class"""
+
+    def __init__(self, width=0, height=0):
+        """method attributes"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+             raise TypeError("height must be an integer")
+        if height < 0:
+             raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """decorator, property height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """to set hieght"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """decorator property width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """to set the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
