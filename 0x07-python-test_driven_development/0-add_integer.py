7adf6ee25c3e8588fdc4a5da6599a78bb962cf0d
jopinta <tatinfa@yahoo.com>
2021-10-06 14:42:58 -0700

    tdd  testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,8 +7,6 @@ def add_integer(a, b=98):
 
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-        return
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-        return
     return int(a+b)
