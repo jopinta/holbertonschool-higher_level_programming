decec2ef1080a9da6c187bc077c0343345bc6d93
jopinta <tatinfa@yahoo.com>
2021-10-06 16:12:24 -0700

    correct output still red

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,8 +11,8 @@ def add_integer(a, b=98):
  that adds
  2 integers"""
 
-    if not isinstance(a, int) and not isinstance(a, float):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
