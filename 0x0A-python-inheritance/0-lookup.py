aea1de54e22aa8cdb76771274860530d689b97c3
jopinta <tatinfa@yahoo.com>
2021-10-12 10:51:34 -0700

    1 n 2, remember indentattion to set attribute

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns the list of available attributes and methods of an object:"""
+
+
+def lookup(obj):
+    """Returns a list object"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""assume that all the elements of the list will be of type int
+"""
+
+
+class MyList(list):
+    """
+class MyList that inherits from list"""
+
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
