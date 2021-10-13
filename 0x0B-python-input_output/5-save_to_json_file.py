fc470f7d459e0a0e7c5d8806ada31017ae96b782
jopinta <tatinfa@yahoo.com>
2021-10-13 13:33:36 -0700

    where are the .json stored?

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""must use the with statement
+"""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON
+"""
+    with open(filename, "w") as new_f:
+        json.dump(my_obj, new_f)
