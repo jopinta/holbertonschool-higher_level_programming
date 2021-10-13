6cf56fad4622dde76899a216fe2a36851e2b6158
jopinta <tatinfa@yahoo.com>
2021-10-12 18:20:22 -0700

    if not indented [AttributeError] 'BaseGeometry' object has no attribute 'area'

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def area(self):
+        """Public instance method"""
+        raise Exception("area() is not implemented")
