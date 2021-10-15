4405ed22d7cdf40d81b75924fb78bab1abaffbd4
jopinta <tatinfa@yahoo.com>
2021-10-15 11:24:07 -0700

    str() should return, the square description

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+
+    def __init__(self, size):
+        Rectangle.integer_validator(self, "size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """method must be implemented"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ str method"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
