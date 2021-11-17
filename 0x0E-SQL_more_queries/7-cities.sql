e3e619a7d090b2cd613a3edfed79a01f636857a4
jopinta <tatinfa@yahoo.com>
2021-11-17 15:55:44 -0800

    adding auro generated cmd

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,3 @@
 -- cities snafran with FK
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-id INT PRIMARY KEY, state_id INT FOREIGN KEY, name VARCHAR(256) NOT NULL);
+id INT AUTO_INCEMENT PRIMARY KEY, state_id INT FOREIGN KEY, name VARCHAR(256) NOT NULL);
