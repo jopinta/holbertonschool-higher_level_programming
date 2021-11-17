4d64760be0b8b5c75aedadc89559a7de75f08fa5
jopinta <tatinfa@yahoo.com>
2021-11-16 18:48:19 -0800

    something ois missing

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,2 @@
 -- data base name passed as argument
-CREATE TABLE IF NOT EXIST second_table AS
-select [id INT, name VARCHAR(256), score INT ]
-FROM hbtn_0c_0
+CREATE TABLE IF NOT EXIST second_table [id INT, name VARCHAR(256), score INT ]
