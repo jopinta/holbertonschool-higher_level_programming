c99d0f0f80ab43ac88e90e44ee968c44a0ceee4e
jopinta <tatinfa@yahoo.com>
2021-11-17 14:39:21 -0800

    it is 0 not o

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- database and user 4 create
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-CREATE USER IF NOT EXISTS 'useer_oD_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON' hbtn_od_2'.* TO 'user_od_2'@'localhost';
+CREATE USER IF NOT EXISTS 'useer_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON' hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
