da81f58bab2fd99ca413692625ed4e551f0dc8cc
jopinta <tatinfa@yahoo.com>
2021-11-17 14:36:39 -0800

    only w select privilage in DB

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- database and user 4 create
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER IF NOT EXISTS 'useer_oD_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON' hbtn_od_2'.* TO 'user_od_2'@'localhost';
