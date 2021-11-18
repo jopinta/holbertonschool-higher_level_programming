1495e5fb05627ea0e8cf2d31a5f157b3114badcd
jopinta <tatinfa@yahoo.com>
2021-11-18 13:03:32 -0800

    using right join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Import the database dump
+SELECT
+tv_shows.title,
+tv_show_genres.genre_id
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
