7e571ea17f000bda0507894092c0ce6d285da748
jopinta <tatinfa@yahoo.com>
2021-11-17 18:49:18 -0800

    fixing parenthesis

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,3 @@
 -- creating a table on the server
-CREATE TABLE IF NOT EXISTS force_name
-(id INT, name NOT NULL VARCHAR(256));
+CREATE TABLE IF NOT EXISTS force_name(
+id INT, name NOT NULL VARCHAR(256));
