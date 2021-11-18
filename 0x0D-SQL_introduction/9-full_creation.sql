776bfb0f92b6bb29af937444936b70f5c5f3e27c
jopinta <tatinfa@yahoo.com>
2021-11-18 07:47:14 -0800

    using insert into

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- data base name passed as argument
 CREATE TABLE IF NOT EXIST second_table [id INT, name VARCHAR(256), score INT ]
-id = 1, name = “John”, score = 10;
-id = 2, name = “Alex”, score = 3;
-id = 3, name = “Bob”, score = 14;
-id = 4, name = “George”, score = 8;
+INSERT INTO secon_table id, name, score) VALUES (1, John, 10);
+INSERT INTO secon_table id, name, score) VALUES (2, Alex, 3);
+INSERT INTO secon_table id, name, score) VALUES (3, Bob, 14);
+INSERT INTO secon_table id, name, score) VALUES (4, George, 8);
