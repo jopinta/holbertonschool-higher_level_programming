6729ab0297084d13baccd99226d830beae45efd2
jopinta <tatinfa@yahoo.com>
2021-11-18 13:10:56 -0800

    in ascending order

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,4 +5,4 @@ tv_show_genres.genre_id
 FROM tv_show_genres
 LEFT OUTER JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
