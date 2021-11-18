acc468fb1b83cf2725fe0e63797c3ea2695a84af
jopinta <tatinfa@yahoo.com>
2021-11-18 07:53:32 -0800

    need quotes around values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- data base name passed as argument
 CREATE TABLE IF NOT EXIST second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO secon_table id, name, score) VALUES (1, John, 10);
-INSERT INTO secon_table id, name, score) VALUES (2, Alex, 3);
-INSERT INTO secon_table id, name, score) VALUES (3, Bob, 14);
-INSERT INTO secon_table id, name, score) VALUES (4, George, 8);
+INSERT INTO secon_table id, name, score) VALUES ('1', 'John', '10');
+INSERT INTO secon_table id, name, score) VALUES ('2', 'Alex', '3');
+INSERT INTO secon_table id, name, score) VALUES ('3', 'Bob', '14');
+INSERT INTO secon_table id, name, score) VALUES ('4', 'George', '8');
