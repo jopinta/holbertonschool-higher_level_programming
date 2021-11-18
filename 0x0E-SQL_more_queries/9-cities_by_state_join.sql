12d01be8fcb50f1a4d4d56cb7d350119348218af
jopinta <tatinfa@yahoo.com>
2021-11-18 12:38:02 -0800

    inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- list cities in DB
-SHOW DATABASE hbtn_0d_usa.cities.id;
-SHOW DATABASE hbtn_0d_usa.cities.name;
-SHOW DATABASE hbtn_0d_usa.states.name;
-SELECT cities FROM hbtn_0d_usa
+SELECT
+cities.id
+cities.name
+states.name
+FROM cities
 JOIN states on cities.state_id=states.id ORDER BY cities.id;
