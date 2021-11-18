b0acfd54df8f46fa6dfcc23d3d76a14fd1279a4d
jopinta <tatinfa@yahoo.com>
2021-11-18 12:59:26 -0800

    oreder asc from genres

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 SELECT
 tv_shows.title,
 tv_show_genres.genre_id
-FROM tv_show
-JOIN tv_shows_genres ON tv_shows.id=tv_show_genres.show_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
