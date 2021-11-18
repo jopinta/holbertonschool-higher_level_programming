3b7d4f249d7d9158b19327c8f86d16a5f7ec3455
jopinta <tatinfa@yahoo.com>
2021-11-18 12:56:17 -0800

    from tv shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT
 tv_shows.title,
 tv_show_genres.genre_id
-FROM tv_show_genres
-JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id;
+FROM tv_show
+JOIN tv_shows_genres ON tv_shows.id=tv_show_genres.show_id;
