38c3437dc5d8cc8852703b6cf983d4434842d87c
jopinta <tatinfa@yahoo.com>
2021-11-18 07:40:14 -0800

    testing records

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
 -- data base name passed as argument
 CREATE TABLE IF NOT EXIST second_table [id INT, name VARCHAR(256), score INT ]
+id = 1, name = “John”, score = 10;
+id = 2, name = “Alex”, score = 3;
+id = 3, name = “Bob”, score = 14;
+id = 4, name = “George”, score = 8;
