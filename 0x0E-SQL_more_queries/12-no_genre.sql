77c2828e2b4700ce7812bd87c2e4fcb9c51f09b3
jopinta <tatinfa@yahoo.com>
2021-11-18 13:15:37 -0800

    trying with outer

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 SELECT
 tv_shows.title,
 tv_show_genres.genre_id
-FROM tv_show_genres
-LEFT JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+FROM tv_show
+LEFT OUTER JOIN tv_shows_genres ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
