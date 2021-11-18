24f4c1bbd8b7143d3807754607fe9975501c5f49
jopinta <tatinfa@yahoo.com>
2021-11-18 13:30:44 -0800

    using subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- listing CALI cities from DB subquery
 SELECT DISTINCT id, name FROM cities
 WHERE state_id=(SELECT id FROM states WHERE name = 'California')
-ORDER BY CITIES.id;
+ORDER BY cities.id;
