aa3d3c0253008b5029fdd487776002df6db4e386
jopinta <tatinfa@yahoo.com>
2021-11-18 13:22:40 -0800

    trying with outer

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id=tv_show_genres.show_id
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
