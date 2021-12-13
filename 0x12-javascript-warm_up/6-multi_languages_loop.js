589c2440a0c0ffb7cfe685dfe529d7f4aa60f5e3
jopinta <tatinfa@yahoo.com>
2021-12-13 10:42:56 -0800

    fixing {

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines
+
+const arrey = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i <= 2; i++) {
+  console.log(arrey[i]);
+}
