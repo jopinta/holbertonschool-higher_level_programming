f06ba4f216397d03318628930760d2f2cc127065
jopinta <tatinfa@yahoo.com>
2021-12-13 11:28:33 -0800

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints x times “C is fun”
+const argv = process.argv;
+const integer = parseInt(argv[2]);
+
+if (!integer) {
+  console.log('Missing number of occurrences');
+} else {
+  for let (i = 0; i < integer; i++) {
+  console.log('C is fun');
+}
+}
