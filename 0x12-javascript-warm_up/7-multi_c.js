8280b3f58e16eaa86e113e4d600faca0edfc1fca
jopinta <tatinfa@yahoo.com>
2021-12-13 11:41:00 -0800

    using is NaN

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@
 const argv = process.argv;
 const integer = parseInt(argv[2]);
 
-if (!integer) {
+if (isNaN(integer)) {
   console.log('Missing number of occurrences');
 } else {
-  for let (i = 0; i < integer; i++) {
-  console.log('C is fun');
-}
+  for (let i = 0; i < integer; i++) {
+    console.log('C is fun');
+  }
 }
