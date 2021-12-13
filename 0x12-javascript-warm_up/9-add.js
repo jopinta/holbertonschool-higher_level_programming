22d17d8dd066abb43c244eaf72432fd94753be1e
jopinta <tatinfa@yahoo.com>
2021-12-13 12:48:00 -0800

    parentesis missing

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,7 @@
 
 const args = process.argv;
 function add (a, b) {
-    return parseInt(a) + parseInt(b);
+  return parseInt(a) + parseInt(b);
 }
-console.log(add(args[2], args[3]);
+
+console.log(add(args[2], args[3]));
