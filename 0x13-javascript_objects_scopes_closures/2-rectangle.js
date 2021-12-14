1abcad6fd668735c20471b5e4ebbe6e5597d4709
jopinta <tatinfa@yahoo.com>
2021-12-14 10:50:24 -0800

    usando return en el condicion if

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// empty class
+const Rectangle = class {
+  constructor (width, height) {
+      if (width <= 0 || width === undefined || height <= 0 || height === undefined) {
+	  return;
+      }
+    this.width = width;
+    this.height = height;
+  }
+};
+module.exports = Rectangle;
