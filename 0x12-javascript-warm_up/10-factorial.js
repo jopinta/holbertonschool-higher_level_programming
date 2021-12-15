18ebc0bc6f57981914705b7678508ce48e5fef7f
jopinta <tatinfa@yahoo.com>
2021-12-15 21:51:20 +0000

    factoria with fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-//computes and prints a factorial
+// computes and prints a factorial
 
 const argv = process.argv;
 const nb = parseInt(argv[2]);
 
-function factorial {
+function factorial (nb) {
   if (nb === 0 || Number.isNaN(nb)) {
     return 1;
   }
-return nb * factorial(nb1);
+  return nb * factorial(nb - 1);
 }
 console.log(factorial(nb));
