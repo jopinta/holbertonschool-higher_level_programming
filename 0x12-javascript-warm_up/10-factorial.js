de3cd7903991f8b2b4e69783bdc8fff029c7acd6
jopinta <tatinfa@yahoo.com>
2021-12-15 21:48:03 +0000

    factoria

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//computes and prints a factorial
+
+const argv = process.argv;
+const nb = parseInt(argv[2]);
+
+function factorial {
+if (nb === 0 || Number.isNAN(nb)) {
+    return 1;
+}
+return nb * factorial(nb1);
+}
+
+console.log(factorial(nb));
