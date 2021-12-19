4e7299f73e928181b4007371a67b54201d6f3988
jopinta <tatinfa@yahoo.com>
2021-12-18 17:34:53 -0800

    if clause on top

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 '''to list objects'''
 
-import sys
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+
     argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
