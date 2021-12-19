f1699cd0d834d2c2b009489cf8069d6cf96cf9d5
jopinta <tatinfa@yahoo.com>
2021-12-18 17:38:20 -0800

    testing with no sys.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], sys.argv[2],
+                           .format(argv[1], argv[2],
                                    argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
