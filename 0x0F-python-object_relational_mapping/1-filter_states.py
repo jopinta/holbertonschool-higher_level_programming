293904a7b7c2e8f73e52295e5981b3fb2a2e991a
jopinta <tatinfa@yahoo.com>
2021-12-18 16:22:43 -0800

    like binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if(__name__ == '__main__'):
 
     cur = conn.cursor()
 
-    request = ("SELECT * FROM states WHERE name LIKE 'N%'")  # my db
+    request = ("SELECT * FROM states WHERE name LIKE BINARY 'N%'")  # my db
     cur.execute(request)
     query_rows = cur.fetchall()
     for row in query_rows:
