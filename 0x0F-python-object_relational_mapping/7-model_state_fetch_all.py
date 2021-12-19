4caae1a55d21c377617df8be4bbb880fc5bd27f9
jopinta <tatinfa@yahoo.com>
2021-12-18 17:21:29 -0800

    argv = sys.argv

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
+    argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
