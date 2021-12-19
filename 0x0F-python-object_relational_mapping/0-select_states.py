c0274b66034ed20f8b9d5beda5319f522c1b8f52
jopinta <tatinfa@yahoo.com>
2021-12-18 16:09:09 -0800

    testing without order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,7 +16,7 @@ if(__name__ == '__main__'):
 
     cur = conn.cursor()
 
-    request = ("SELECT * FROM states order by id")  # cur.execute(request)
+    request = ("SELECT * FROM states")  # cur.execute(request)
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
