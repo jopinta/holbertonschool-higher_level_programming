637e880d45296058c7ef1296c1cef693b25430fa
jopinta <tatinfa@yahoo.com>
2021-12-18 18:05:01 -0800

    state defined l17

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''to list objects'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).order_by(State.id).first()
+    if not state:
+        print('Nothing')
+    else:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
