044ca2a96e55f11cc7762b879eea913b2bf21ea0
jopinta <tatinfa@yahoo.com>
2021-12-18 18:14:15 -0800

    testing returning more state

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''to list objects'''
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    argv = sys.argv
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.name.like("%a%"))\
+                                     .order_by(State.id).all():   
+        print("{}: {}".format(state.id, state.name))
+    session.close()
