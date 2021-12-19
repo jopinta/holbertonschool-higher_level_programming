79ceb1a776147de6e45de04533a69442eb88f2e5
jopinta <tatinfa@yahoo.com>
2021-12-18 17:51:49 -0800

    indent fixed

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,11 +9,11 @@ from sqlalchemy import create_engine
 if __name__ == '__main__':
     argv = sys.argv
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2],
-                                   argv[3]), pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
-session.close()
+        session.close()
