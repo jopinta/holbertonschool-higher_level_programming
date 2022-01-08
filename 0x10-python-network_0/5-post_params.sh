82b9c31a57d823d98cc1ffee96201cc3d449002d
jopinta <tatinfa@yahoo.com>
2022-01-08 13:55:01 -0800

    POST parameters splitted

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  request that the origin server accept the entity
-curl -sX "$1" POST -d "email:test@gmail.com" -d "subject:I will always be here for PLD"
+curl -sX POST -d "email:test@gmail.com" -d "subject:I will always be here for PLD" "$1"
