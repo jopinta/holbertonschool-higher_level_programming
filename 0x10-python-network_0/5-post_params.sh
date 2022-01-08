b42eb34511cedb1475255e12624a8c9e3f1a4b58
jopinta <tatinfa@yahoo.com>
2022-01-08 13:51:19 -0800

    POST parameters swap

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  request that the origin server accept the entity
-curl -s "$1" -X POST -d "email:test@gmail.com&subject:I will always be here for PLD"
+curl -sX "$1" POST -d "email:test@gmail.com&subject:I will always be here for PLD"
