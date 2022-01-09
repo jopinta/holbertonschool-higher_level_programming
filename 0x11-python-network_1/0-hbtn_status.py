dbf87928541ee33997f2d0daa381c7a42138411b
jopinta <tatinfa@yahoo.com>
2022-01-09 12:53:03 -0800

    fixed no pycode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-# script that fetches an url
+'''script that fetches an url'''
 
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body))
-    print("\t- content: {}".format(type(body))
-    print("\t- utf8: {}".format(type(body.decode("utf8"))
+if __name__ == '__main__':
+
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(type(body.decode("utf-8")))
