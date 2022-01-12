218c93e1ac595cc781bc5848dbae94ffe2eb53aa
jopinta <tatinfa@yahoo.com>
2022-01-12 11:05:29 -0800

    sys for argv 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@
 import requests
 import sys
 if __name__ == "__main__":
-    quest = requests.post(sys.argv[1], data={'email': argv[2]})
+    quest = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(quest.text)
