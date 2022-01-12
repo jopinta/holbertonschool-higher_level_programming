d6611009638d5270e8d10d37dc11b4cbbb9bda39
jopinta <tatinfa@yahoo.com>
2022-01-12 11:01:29 -0800

    post header

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' displays the body of the response.'''
+
+import requests
+import sys
+if __name__ == "__main__":
+    quest = requests.post(sys.argv[1], data={'email': argv[2]})
+    print(quest.text)
+
+    
