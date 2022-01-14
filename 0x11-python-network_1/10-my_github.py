9b53b2b3d65b12827671f233640200458f8a2567
jopinta <tatinfa@yahoo.com>
2022-01-13 16:05:30 -0800

    pushing 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' displays the body of the response.'''
+
+import requests
+import sys
+if __name__ == "__main__":
+    quest = requests.get(sys.argv[1])
+    if quest.status_code < 400:
+        print(quest.text)
+    else:
+        print("Error code: {}".format(quest.status_code))
