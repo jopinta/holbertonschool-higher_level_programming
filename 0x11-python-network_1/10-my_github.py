3421df6bc7fc2fb357046bf05b852ccd8b53150b
jopinta <tatinfa@yahoo.com>
2022-01-13 16:19:45 -0800

    10 fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 ''' displays the body of the response.'''
 
-import requests
+from requests import get
 import sys
 if __name__ == "__main__":
-    quest = get("https://api.github.com/user", auth=(sys.arv[1], sys.argv[2]))
+    quest = get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
     try:
         print(quest.json().get('id'))
     except:
