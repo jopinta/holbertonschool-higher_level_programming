1328e6674a7527b0e523b03ad8add0b07a2ca1d6
jopinta <tatinfa@yahoo.com>
2022-02-01 18:52:48 -0800

    const val

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+const val = argv[2];
 const argv = process.argv;
 const quest = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + val;
 quest(url, function (error, response, body) {
   if (error) throw error;
   console.log(JSON.parse(body).title);
