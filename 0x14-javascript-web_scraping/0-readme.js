21b3b3cf86fc67243b8a420575b652be4d1b4d45
jopinta <tatinfa@yahoo.com>
2022-02-01 17:49:38 -0800

    unexpected string corrected

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argv process.argv;
+const argv = process.argv;
 const fs = require('fs');
-fs.readFile(argv[2], 'utf8' (err, data) => {
+fs.readFile(argv[2], 'utf8', (err, data) => {
     if (err) throw err
     console.log(data);
 });
