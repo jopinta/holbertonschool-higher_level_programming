1c107bceea0a77e385c783298b80bd789d07403c
jopinta <tatinfa@yahoo.com>
2022-02-09 13:11:53 -0800

    leia Organa

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,5 @@
-$(document).ready(function(){
-    $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(){
-      const name = data.name;
-      $('div'.text(name);
-  })
-})
+$(document).ready(function () {
+    $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+      $('div'.text(data.name);
+ });
+});
