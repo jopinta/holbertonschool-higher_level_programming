bc2cd503f3c76825919def1449be4532dad39c6d
jopinta <tatinfa@yahoo.com>
2022-02-09 13:01:15 -0800

    now at 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(){
+      const element = data.result;
+      for (let x = 0; i < results.length; i++) {
+	$('div'.text(name);
+  })
+})
