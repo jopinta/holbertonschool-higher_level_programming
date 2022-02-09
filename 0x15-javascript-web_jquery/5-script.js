983ef0096290bd9fda64d6642f0ba113fbf2c2dc
jopinta <tatinfa@yahoo.com>
2022-02-09 12:43:46 -0800

    adding item

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $('div').click(function(){
-    'UL.my_list').append(<li>Item</li>);
-  })
-})
+$(document).ready(function() {
+  $('div').click(function() {
+    $('UL.my_list').append(<li>Item</li>);
+  });
+});
